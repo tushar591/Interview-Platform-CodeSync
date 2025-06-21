77f513d06c8d30288386b82199a3abed89997f63
TUSHAR <tushar9445958996@gmail.com>
2025-06-21 16:51:10 +0530

    Done with svix and mutations for DB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="m-10">
-      
+      <SignInButton>
+        <Button>Sign in</Button>
+      </SignInButton>
     </div>
   );
 }
