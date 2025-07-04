d979492e9df5b59fb3e223e62907e07cafa219a3
TUSHAR <tushar9445958996@gmail.com>
2025-07-04 17:07:56 +0530

    Added clerk auth in websocket as aud

--- src/components/ui/providers/ConvexClerkProvider.tsx
+++ src/components/ui/providers/ConvexClerkProvider.tsx
@@ -1,17 +1,25 @@
-"use client"
+"use client";
 import { ClerkProvider, useAuth } from "@clerk/nextjs";
-import  {ConvexReactClient} from "convex/react";
+import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import React from "react";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const convex = new ConvexReactClient(
+  process.env.NEXT_PUBLIC_CONVEX_URL!
+);
 
-export default function ConvexClerkProvider({children}:{children : React.ReactNode}) {
-    return(
-    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+export default function ConvexClerkProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+    >
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
         {children}
       </ConvexProviderWithClerk>
     </ClerkProvider>
-    )
-};
+  );
+}
