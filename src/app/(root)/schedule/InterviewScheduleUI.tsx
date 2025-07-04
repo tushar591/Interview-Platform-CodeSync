0e2b589ea8a06c024ff68c6cf7c7c87464c82dcc
TUSHAR <tushar9445958996@gmail.com>
2025-07-04 17:22:42 +0530

    Completed Schedule Page

--- src/app/(root)/schedule/InterviewScheduleUI.tsx
+++ src/app/(root)/schedule/InterviewScheduleUI.tsx
@@ -26,6 +26,7 @@ import { Loader2Icon, XIcon } from "lucide-react";
 import { Calendar } from "@/components/ui/calendar";
 import { TIME_SLOTS } from "@/constants";
 import UserInfo from "@/components/ui/UserInfo";
+import MeetingCard from "@/components/ui/MeetingCard";
 
 function InterviewScheduleUI() {
   const client = useStreamVideoClient();
@@ -288,7 +289,21 @@ function InterviewScheduleUI() {
       </div>
 
       {/* LOADING STATE & MEETING CARDS */}
-      
+      {!interviews ? (
+        <div className="flex justify-center py-12">
+          <Loader2Icon className="size-8 animate-spin text-muted-foreground" />
+        </div>
+      ) : interviews.length > 0 ? (
+        <div className="spacey-4">
+          <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+            {interviews.map((interview) => (
+              <MeetingCard key={interview._id} interview={interview} />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className="text-center py-12 text-muted-foreground">No interviews scheduled</div>
+      )}
     </div>
   );
 }
